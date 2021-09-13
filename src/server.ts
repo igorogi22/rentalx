f0a82c42cce71ba267e2702fe1cdaef31068dec4
Igor Vinicius <igorvinicius.oli@gmail.com>
2021-09-12 21:05:35 -0300

    feat: import categories routes

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,10 @@
 import express from 'express';
+import { categoriesRoutes } from './routes';
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (request, response) => {
-    return response.json({ message: "Hello world!!" });
-})
-
-app.post("/courses", (request, response) => {
-    const { name } = request.body;
-
-    return response.json({ name });
-})
+app.use(categoriesRoutes);
 
 app.listen(3000, () => console.log("Server is running"));
