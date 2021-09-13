30d2ade5f298dd790b71ea9dd50f138a8f0fb9d1
Igor Vinicius <igorvinicius.oli@gmail.com>
2021-09-13 09:53:02 -0300

    refactor: eslint

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,27 +1,28 @@
 import { Router } from "express";
+
 import { CategoriesRespository } from "../repositories";
 
 const categoriesRoutes = Router();
 const categoriesrepository = new CategoriesRespository();
 
 categoriesRoutes.post("/", (request, response) => {
-    const { name, description } = request.body;
+  const { name, description } = request.body;
 
-    const categoryAlreadyExists = categoriesrepository.findByName(name);
+  const categoryAlreadyExists = categoriesrepository.findByName(name);
 
-    if (categoryAlreadyExists) {
-        return response.status(400).json({ error: "Category already exists!!" });
-    }
+  if (categoryAlreadyExists) {
+    return response.status(400).json({ error: "Category already exists!!" });
+  }
 
-    categoriesrepository.create({ name, description });
+  categoriesrepository.create({ name, description });
 
-    return response.status(201).send();
-})
+  return response.status(201).send();
+});
 
 categoriesRoutes.get("/", (request, response) => {
-    const allCategories = categoriesrepository.list();
+  const allCategories = categoriesrepository.list();
 
-    return response.status(200).json(allCategories);
-})
+  return response.status(200).json(allCategories);
+});
 
 export { categoriesRoutes };
