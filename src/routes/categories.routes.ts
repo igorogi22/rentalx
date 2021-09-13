f5a49755a1c87b2544a384b43f30aec7b174ab99
Igor Vinicius <igorvinicius.oli@gmail.com>
2021-09-12 22:07:45 -0300

    feat: added validate if category exists

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -7,8 +7,14 @@ const categoriesrepository = new CategoriesRespository();
 categoriesRoutes.post("/", (request, response) => {
     const { name, description } = request.body;
 
+    const categoryAlreadyExists = categoriesrepository.findByName(name);
+
+    if (categoryAlreadyExists) {
+        return response.status(400).json({ error: "Category already exists!!" });
+    }
+
     categoriesrepository.create({ name, description });
-    
+
     return response.status(201).send();
 })
 
