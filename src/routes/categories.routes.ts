38995c49b6fa56db8f62ecb3197733b15c245911
Igor Vinicius <igorvinicius.oli@gmail.com>
2021-09-12 21:05:07 -0300

    feat: implemented 'create a new categorie' and configure a categories route

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import  { v4 as uuidV4 } from "uuid";
+
+const categoriesRoutes = Router();
+
+const categories = [];
+
+categoriesRoutes.post("/categories", (request, response) => {
+    const { name, description } = request.body;
+
+    categories.push({
+        id: uuidV4(),
+        name,
+        description
+    });
+
+    return response.status(201).send();
+})
+
+export { categoriesRoutes };
