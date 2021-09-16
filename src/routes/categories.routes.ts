902d47b2642e169c7ac3acd9077ed0562174ff58
Igor Vinicius <igorvinicius.oli@gmail.com>
2021-09-16 10:14:48 -0300

    refactor: change 'categories.routes' to use 'ListCategoriesController'

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,19 +1,18 @@
 import { Router } from "express";
 
-import { CategoriesRespository } from "../modules/cars/repositories";
-import { createCategoryController } from "../modules/cars/useCases";
+import {
+  createCategoryController,
+  listCategoriesController,
+} from "../modules/cars/useCases";
 
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoriesRespository();
 
 categoriesRoutes.post("/", (request, response) => {
   return createCategoryController.handler(request, response);
 });
 
 categoriesRoutes.get("/", (request, response) => {
-  const allCategories = categoriesRepository.list();
-
-  return response.status(200).json(allCategories);
+  return listCategoriesController.handle(request, response);
 });
 
 export { categoriesRoutes };
