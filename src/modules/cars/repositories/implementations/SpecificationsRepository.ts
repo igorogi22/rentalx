8bbfbc53a52a337505384f9ce9581340325695cd
Igor Vinicius <igorvinicius.oli@gmail.com>
2021-09-16 12:34:38 -0300

    feat: add method list to return all specifications

--- src/modules/cars/repositories/implementations/SpecificationsRepository.ts
+++ src/modules/cars/repositories/implementations/SpecificationsRepository.ts
@@ -38,6 +38,10 @@ class SpecificationsRepository implements ISpecificationsRepository {
 
     return specification;
   }
+
+  list(): Specification[] {
+    return this.specifications;
+  }
 }
 
 export { SpecificationsRepository };
