97f51b812d40f6c3bd6c09277931b8ffd7488ef9
Igor Vinicius <igorvinicius.oli@gmail.com>
2021-09-16 12:13:19 -0300

    refactor: change 'CreateSpecificationService' to 'CreateSpecificationUseCase' in modules/cars/useCases

--- src/modules/cars/useCases/CreateSpecification/CreateSpecificationUseCase.ts
+++ src/modules/cars/useCases/CreateSpecification/CreateSpecificationUseCase.ts
@@ -1,10 +1,11 @@
-import { ISpecificationsRepository } from "../repositories";
+import { ISpecificationsRepository } from "../../repositories";
 
 interface IRequest {
   name: string;
   description: string;
 }
-class CreateSpecificationService {
+
+class CreateSpecificationUseCase {
   constructor(private specificationRepository: ISpecificationsRepository) {}
 
   execute({ name, description }: IRequest): void {
@@ -22,4 +23,4 @@ class CreateSpecificationService {
   }
 }
 
-export { CreateSpecificationService };
+export { CreateSpecificationUseCase };
