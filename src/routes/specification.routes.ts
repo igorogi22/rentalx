b4f4fd90a083f310ae1f726a5844f4bcb9ead588
Igor Vinicius <igorvinicius.oli@gmail.com>
2021-09-16 12:36:13 -0300

    feat: add list specification route

--- src/routes/specification.routes.ts
+++ src/routes/specification.routes.ts
@@ -1,11 +1,18 @@
 import { Router } from "express";
 
-import { createSpecificationsController } from "../modules/cars/useCases";
+import {
+  createSpecificationsController,
+  listSpecificationsController,
+} from "../modules/cars/useCases";
 
 const specificationRoutes = Router();
 
 specificationRoutes.post("/", (request, response) => {
   return createSpecificationsController.handle(request, response);
+});
+
+specificationRoutes.get("/", (request, response) => {
+  return listSpecificationsController.handle(request, response);
 });
 
 export { specificationRoutes };
