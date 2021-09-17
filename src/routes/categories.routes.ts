45ba759277d1717ee52338a90c5430cd28562a1f
Igor Vinicius <igorvinicius.oli@gmail.com>
2021-09-17 17:58:45 -0300

    feat: add import categories route

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,11 +1,17 @@
 import { Router } from "express";
+import multer from "multer";
 
 import {
   createCategoryController,
+  importCategoriesController,
   listCategoriesController,
 } from "../modules/cars/useCases";
 
 const categoriesRoutes = Router();
+
+const upload = multer({
+  dest: "./tmp",
+});
 
 categoriesRoutes.post("/", (request, response) => {
   return createCategoryController.handler(request, response);
@@ -13,6 +19,10 @@ categoriesRoutes.post("/", (request, response) => {
 
 categoriesRoutes.get("/", (request, response) => {
   return listCategoriesController.handle(request, response);
+});
+
+categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
+  return importCategoriesController.handle(request, response);
 });
 
 export { categoriesRoutes };
