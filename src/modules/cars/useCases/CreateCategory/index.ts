3f8c3f34206695cc7a22100ea0ed51ce35a78ae5
Igor Vinicius <igorvinicius.oli@gmail.com>
2021-09-28 11:00:47 -0300

    refactor: index of category use case to function

--- src/modules/cars/useCases/CreateCategory/index.ts
+++ src/modules/cars/useCases/CreateCategory/index.ts
@@ -2,10 +2,14 @@ import { CategoriesRespository } from "../../repositories";
 import { CreateCategoryController } from "./CreateCategoryController";
 import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
 
-const categoriesRepository = CategoriesRespository.getInstance();
-const createCategoryUseCase = new CreateCategoryUseCase(categoriesRepository);
-const createCategoryController = new CreateCategoryController(
-  createCategoryUseCase
-);
+const createCategoryController = (): CreateCategoryController => {
+  const categoriesRepository = new CategoriesRespository();
+  const createCategoryUseCase = new CreateCategoryUseCase(categoriesRepository);
+  const createCategoryController = new CreateCategoryController(
+    createCategoryUseCase
+  );
+
+  return createCategoryController;
+};
 
 export { createCategoryController };
