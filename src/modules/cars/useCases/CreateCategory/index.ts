f2cc6829868b90d989b11bece511eaee18877fc0
Igor Vinicius <igorvinicius.oli@gmail.com>
2021-09-29 09:28:45 -0300

    refactor: export all at create category

--- src/modules/cars/useCases/CreateCategory/index.ts
+++ src/modules/cars/useCases/CreateCategory/index.ts
@@ -1,15 +1,2 @@
-import { CategoriesRespository } from "../../repositories";
-import { CreateCategoryController } from "./CreateCategoryController";
-import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
-
-const createCategoryController = (): CreateCategoryController => {
-  const categoriesRepository = new CategoriesRespository();
-  const createCategoryUseCase = new CreateCategoryUseCase(categoriesRepository);
-  const createCategoryController = new CreateCategoryController(
-    createCategoryUseCase
-  );
-
-  return createCategoryController;
-};
-
-export { createCategoryController };
+export * from "./CreateCategoryController";
+export * from "./CreateCategoryUseCase";
