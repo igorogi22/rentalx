effc9cebcbc86c7221bcde7db5ad1119888b368b
Igor Vinicius <igorvinicius.oli@gmail.com>
2021-10-03 21:09:07 -0300

    feat: add find user by email at user repository interface

--- src/modules/accounts/repositories/IUsersRepository.ts
+++ src/modules/accounts/repositories/IUsersRepository.ts
@@ -1,7 +1,9 @@
 import { ICreateUserDto } from "../dtos";
+import { User } from "../entities";
 
 interface IUsersRepository {
   create(data: ICreateUserDto): Promise<void>;
+  findByEmail(email: string): Promise<User>;
 }
 
 export { IUsersRepository };
