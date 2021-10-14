fe704b99a72c5c91962d3b28f1a94fdf67713821
Igor Vinicius <igorvinicius.oli@gmail.com>
2021-10-13 23:11:00 -0300

    feat: add upload avatar route

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,11 +1,26 @@
 import { Router } from "express";
+import multer from "multer";
 
+import { uploadConfig } from "../config";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 import { CreateUserController } from "../modules/accounts/useCases/createUser";
+import { UpdateUserAvatarController } from "../modules/accounts/useCases/updateUserAvatar";
 
 const usersRoutes = Router();
 
+const uploadAvatar = multer(uploadConfig.upload("./tmp/avatar"));
+
 const createUserController = new CreateUserController();
 
+const updateUserAvatarController = new UpdateUserAvatarController();
+
 usersRoutes.post("/", createUserController.handler);
+
+usersRoutes.patch(
+  "/avatar",
+  ensureAuthenticated,
+  uploadAvatar.single("avatar"),
+  updateUserAvatarController.handler
+);
 
 export { usersRoutes };
